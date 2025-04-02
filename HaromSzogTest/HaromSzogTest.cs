7b4a6da61f6843de2496f0ba4078ca1ce39d0bd3
Bence <b3niib@gmail.com>
2025-04-02 13:10:16 +0200

    Add project files.

---
+++ HaromSzogTest/HaromSzogTest.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using HaromSzogekkree;
+
+namespace HaromSzogTest
+{
+    [TestClass]
+    public class HaromSzogTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var ell = new HaromSzogekre();
+            int a = 5;
+            int b = 5;
+            int c = 5;
+
+            string eredmeny = ell.HaromSzogek(a, b, c);
+
+            Assert.AreEqual("szabályos háromszög", eredmeny);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var ell = new HaromSzogekre();
+            int a = 3;
+            int b = 5;
+            int c = 5;
+
+            string eredmeny = ell.HaromSzogek(a, b, c);
+
+            Assert.AreEqual("egyenlő szárú háromszög", eredmeny);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var ell = new HaromSzogekre();
+            int a = 2;
+            int b = 3;
+            int c = 4;
+
+            string eredmeny = ell.HaromSzogek(a, b, c);
+
+            Assert.AreEqual("általános háromszög", eredmeny);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var ell = new HaromSzogekre();
+            int a = 2;
+            int b = 3;
+            int c = 10;
+
+            string eredmeny = ell.HaromSzogek(a, b, c);
+
+            Assert.AreEqual("nem háromszög, ha nem felel meg a hárömszög-egyenlőség tételének", eredmeny);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var ell = new HaromSzogekre();
+            int a = 0;
+            int b = 2;
+            int c = 5;
+
+            string eredmeny = ell.HaromSzogek(a, b, c);
+
+            Assert.AreEqual("nem háromszög, mert az egyik oldal nulla", eredmeny);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var ell = new HaromSzogekre();
+            int a = 2;
+            int b = -5;
+            int c = 1;
+
+            string eredmeny = ell.HaromSzogek(a, b, c);
+
+            Assert.AreEqual("nem háromszög, mert az egyik oldal negatív", eredmeny);
+        }
+    }
+}
---
+++ HaromSzogTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HaromSzogTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HaromSzogTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e5810ab-b1d4-4272-b77a-688fce20e483")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
